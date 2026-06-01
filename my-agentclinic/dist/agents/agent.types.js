7be62376cfac3acc85619b3c77e0ef582c9e023f
singhramkumar <singh.ramkumar@hotmail.com>
2026-06-01 04:48:46 -0700

    update gitingore

--- my-agentclinic/dist/agents/agent.types.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AgentStatus = void 0;
-var AgentStatus;
-(function (AgentStatus) {
-    AgentStatus["Active"] = "active";
-    AgentStatus["Inactive"] = "inactive";
-    AgentStatus["InTherapy"] = "in-therapy";
-})(AgentStatus || (exports.AgentStatus = AgentStatus = {}));
